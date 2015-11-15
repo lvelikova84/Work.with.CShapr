32b973162ad702a028d02819116e97b4767176b4
Лилия Великова <lvelikova84@abv.bg>
2015-11-15 08:42:16 +0200

    Работа с Double

---
+++ Work.Csharp/Work.with.double/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Work.withdouble
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double a = 0, b = 0;
+			a = 1.345;
+			string d = "1.687"; b = Convert.ToDouble(d); 
+			Console.WriteLine(a+b);
+		}
+	}
+}
