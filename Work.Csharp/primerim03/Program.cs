59cbe82b876ae5b9cdf23c41f9c76d5a5f6a857d
Лилия Великова <lvelikova84@abv.bg>
2015-11-15 22:00:09 +0200

    Пример 03-05 - Булеви операции

---
+++ Work.Csharp/primerim03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace primerim03
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+//			Имаме променливите ivan и georgi които обозначават кой от тях присъства в нашето въображаемо събитие. 
+//			Нека първата ни bool променлива да е "true" (вярна) когато и двамата присъстват
+//			Нека втората ни променлива да е вярна когато поне един присъства
+//			Нека третата ни променлива да е вярна когато нито един не присъства
+//			Нека да видим какво се е получило
+
+			bool ivan = true;
+			bool georgi = false;
+			bool i_dvamata = ivan && georgi; 
+			bool pone_edin = ivan || georgi; 
+			bool nito_edin = !pone_edin; 
+			Console.WriteLine(i_dvamata + " " + pone_edin + " " +nito_edin);
+
+		
+		}
+	}
+}
