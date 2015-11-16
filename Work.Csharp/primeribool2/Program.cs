be3abf0193c350e7cb8ff063b318fd98230ada8b
Лилия Великова <lvelikova84@abv.bg>
2015-11-16 09:40:33 +0200

    още примери с if

---
+++ Work.Csharp/primeribool2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace primeribool2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int a=4,b=1,c=5,d=4;
+			bool x=false;
+			if (a>b) {
+				if (c>d) {
+					if (d>d) {
+						x=true; 
+					} else {
+						if (a>3) {
+							x=true; 
+						} 
+					}
+				} else {
+					x=true;
+				}
+			} 
+
+			if (x) Console.WriteLine("Има"); 
+			else Console.WriteLine("Няма");
+
+		}
+	}
+}
